9fcc7e9741e83393611151cf47c70c563e659291
jeremy <jeremyhoarau44@gmail.com>
2023-10-04 11:16:01 +0400

    ajout d'un fichier vueReservations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./vue/*.{html,js,php}",
+    "./**/*.{html,js,php}",
+    "./*{html,js,php}",
   ],
   theme: {
     extend: {
