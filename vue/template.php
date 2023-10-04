2827d1526677adb379bf1110f6892ba3bdb6efda
VITRYBenjamin <144926848+VITRYBenjamin@users.noreply.github.com>
2023-10-04 09:14:17 +0400

    Update template.php

--- vue/template.php
+++ vue/template.php
@@ -1,15 +1,100 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
+    <title>Your Website Title</title>
+    <meta name="description" content="Description of your website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../css/output.css">
-    <link rel="preconnect" href="fonts/NotoSansSC-VariableFont_wght.ttf">
-    <link rel="preconnect" href="fonts/Roboto-Bold.ttf">
+    
+    <!-- Ajout d'un potentielle fichier CSS personnalisé -->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    
+    <!-- Ajout de Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <style>
+        /* Bar verticale pour le menu */
+        .nav-item:not(:last-child) {
+            border-right: 1px solid black; /* bar verticale */
+            padding-right: 10px; /* ajustement de l'espace pour les bars */
+        }
+
+        /* Css pour les menu déroulant */
+        .dropdown-menu {
+            position: relative;
+            display: inline-block;
+        }
+
+        /* CSS pour le contenue du menu déroulant */
+        .dropdown-content {
+            display: ;
+            position: absolute;
+            background-color: #fff;
+            min-width: 100px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-menu:hover .dropdown-content {
+            display: block;
+        }
+
+        /* CSS pour le contenue principale */
+        .main-content {
+            max-width: 800px; /* Ajustement de la taille */
+            margin: 0 auto; /* Centré le bloc d'information principale */
+            padding: 20px; /* Ajout du padding pour l'espacement du contenue */
+        }
+    </style>
+
 </head>
 <body>
-    
-    
+    <header>
+        <!-- Logo -->
+        <div class="logo">
+            <img src="logo.png" alt="Your Logo" width="100">
+        </div>
+        
+        <!-- Nom de l'entreprise -->
+        <div class="bg-gray-300 py-2 text-center text-black">
+            <div class="bg-gray-300 inline-block px-4 py-2 rounded-md">
+                <h1 class="text-2xl font-semibold">Le Nom de Votre Entreprise</h1>
+            </div>
+        </div>
+
+        <!-- Bar de navigation -->
+        <nav class="bg-red-600 container mx-auto">
+            <div class="py-4 flex justify-center items-center">
+                <ul class="flex space-x-4 text-black">
+                    <li class="nav-item"><a href="#action?accueil">Accueil</a></li>
+                    <li class="nav-item"><a href="#action?reserver">Réserver</a></li>
+                    <li class="nav-item"><a href="#action?contacter">Nous Contacter</a></li>
+                    <li class="nav-item"><a href="#action?client">Espace Client</a></li>
+                    <li class="nav-item group relative">
+                        <!-- Menu déroulant Langue -->
+                        <a href="#action?" class="group-hover:text-gray-300">Langue</a>
+                        <ul class="dropdown-content hidden absolute right-0 mt-2 py-2 bg-black border border-gray-200 rounded-lg">
+                            <li><a href="#action?langue=francais" class="block px-4 py-2 text-gray-800">Français</a></li>
+                            <li><a href="#action?langue=english" class="block px-4 py-2 text-gray-800">English</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Conteneur du contenue principale -->
+    <div class="main-content mx-auto mt-8 p-4">
+        <main>
+            <section>
+                <h2 class="text-3xl font-semibold">Bienvenue sur Votre Site Web</h2>
+                <p>Ceci est le contenu principal de votre site web.</p>
+            </section>
+        </main>
+    </div>
+
+    <footer class="text-center py-4 bg-gray-300">
+        <p>&copy; 2023 Le Nom de Votre Site Web</p>
+    </footer>
 </body>
 </html>
