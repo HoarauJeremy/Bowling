24f6f60506c2846a5e3f3f7f031f0b51b0cf6e4d
Tetrassky <tetrasky.bat@gmail.com>
2023-10-13 07:12:04 +0400

    Ajout page et logo erreur

--- vue/template.php
+++ vue/template.php
@@ -118,12 +118,12 @@
     <!-- Conteneur du contenue principale -->
     <div class="w-full mt-8 p-4">
         <main>
-            <!-- Nom de l'entreprise -->
+            <!-- Nom de l'entreprise 
             <div class="hidden md:flex md:bg-gray-300 md:py-2 md:text-center md:text-black md:w-2/5 md:h-14 md:justify-center md:items-center">
-            <!-- <div class="bg-gray-300 inline-block px-4 py-2 rounded-md"> -->
-                <h1 class="text-2xl font-semibold">Le Bowling du Front de Mer</h1>
-            <!-- </div> -->
-            </div>
+            <div class="bg-gray-300 inline-block px-4 py-2 rounded-md"> 
+            <h1 class="text-2xl font-semibold">Le Bowling du Front de Mer</h1>
+            <</div>
+            </div> -->
 
             <section>
                 <article>
--- vue/vueErreur.php
+++ vue/vueErreur.php
@@ -1,9 +1,9 @@
 <?php
+    $titre = "Bowling du Front de Mer - Erreur"; 
+    $contenu .= "<div class='flex flex-col items-center text-center my-20'>";
+        $contenu .= "<h1 class='font-Roboto font-extrabold text-4xl'>La page recherchée n'a pas été trouvée.</h1>";
+        $contenu .= "<img src='media/images/LogoErreur.png' alt='Your Logo' width='600' height='600' class=''>";
+    $contenu .= "</div>";
 
-    $titre = "Accueil";
-
-    $contenu .= "<h1 class=''>";
-    
     include "template.php";
-
 ?>
