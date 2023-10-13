fcc75041a22fbcee6a152273c8520d84eef218f3
Tetrassky <tetrasky.bat@gmail.com>
2023-10-13 16:33:42 +0400

    Modification logo Erreur et vue

--- vue/vueErreur.php
+++ vue/vueErreur.php
@@ -1,9 +1,13 @@
 <?php
+    
+    $erreur = $_SERVER["REDIRECT_STATUS"];
+    
     $titre = "Bowling du Front de Mer - Erreur"; 
-    $contenu .= "<div class='flex flex-col items-center text-center my-20'>";
+    $contenu .= "<div class='flex flex-col items-center text-center mt-20 mb-0'>";
         $contenu .= "<h1 class='font-Roboto font-extrabold text-4xl'>La page recherchée n'a pas été trouvée.</h1>";
-        $contenu .= "<img src='media/images/LogoErreur.png' alt='Your Logo' width='600' height='600' class=''>";
+        $contenu .= "<img src='media/images/LogoErreur.png' alt='Your Logo' width='450' height='450' class=''>";
+        $contenu .= "<button class='font-Roboto font-extrabold text-2xl bg-red-800 text-white my-3.5 px-10 h-16 w-auto border-none cursor-pointer rounded-full hover:bg-red-900 hover:border-solid hover:border-spacing-0.5'><a href='index.php'>Retour</a></button>";
     $contenu .= "</div>";
 
     include "template.php";
-?>
+?>  
