64e05b8be4a25de63e8710847862cf38a644e867
Tetrassky <tetrasky.bat@gmail.com>
2023-11-14 15:43:48 +0400

    Couche de bébé

---
+++ controler/controleurConnexion.php
@@ -0,0 +1,35 @@
+<?php
+require_once File::build_path(array("vue", "vueConnexion.php"));
+class ControleurConnexion
+{
+    public function getUtilisateur($username, $password, $cnx)
+    {
+        // Lignes pour empêcher l'insertion de code SQL
+        $username = mysqli_real_escape_string($cnx,htmlspecialchars($_POST['username'])); 
+        $password = mysqli_real_escape_string($cnx,htmlspecialchars($_POST['password']));
+    
+        if($username !== "" && $password !== "")
+        {
+            $sql = "SELECT count(*) FROM Utilisateur where LoginUser = '".$username."' AND MdpUser = '".$password."' ";
+            $rqt = $this->cnx->prepare($sql);
+            $rqt->execute();
+            $exec_requete = mysqli_query($cnx,$rqt);
+            $reponse = mysqli_fetch_array($exec_requete);
+    
+            $count = $reponse['count(*)'];
+            if($count!=0) // nom d'utilisateur et mot de passe correctes
+                {
+                    $_SESSION['CONNECTER'] = "OK";
+                    header('Location: vueProfil.php');
+                    echo "Connexion réussie";
+                }
+            else
+                {
+                    header('Location: vueConnexion.php?erreur=1'); // utilisateur ou mot de passe incorrect
+            }
+        }
+        mysqli_close($cnx);
+    }
+}
+
+?>
