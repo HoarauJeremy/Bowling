68517cc2f54211e6cd2c2e2a5ac1e2e4560a72b8
Tetrassky <tetrasky.bat@gmail.com>
2023-11-21 20:39:58 +0400

    Vue profil

--- vue/vueProfil.php
+++ vue/vueProfil.php
@@ -2,7 +2,7 @@
     if ($_SESSION['CONNECTER']=="OK") 
     {
         $titre = "Bowling du Front de Mer - Profil";
-        $contenu = "<h1>BONJOUR</h1>";
+        $contenu = "<h1 class='font-Roboto text-4xl text-center'>Bienvenue <strong>$username</strong> !</h1>";
     } else {
         include("vue/vueAcceuil");
     }
