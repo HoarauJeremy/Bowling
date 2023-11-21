93847833809518d0e86b4d7f534369b7e51c8b7b
Tetrassky <tetrasky.bat@gmail.com>
2023-11-21 11:04:00 +0400

    Connexion terminée - Profil

--- vue/template.php
+++ vue/template.php
@@ -107,15 +107,28 @@
             <button onclick="toggleNav()" aria-label="toggle button" aria-expanded="false" id="menu-btn" class="cursor-pointer w-14 md:hidden">
                 <img src="media/images/burger-menu-svgrepo-com.svg" alt="" />
             </button>
-
-            <a href="?action=connexion" class="flex md:ml-10 md:mr-16">
-                <span id="logo" class="h-auto w-auto py-2 ">
+<?php
+    if (isset($_SESSION['CONNECTER']) != "OK")
+        {
+           echo '<a href="?url=Connexion/PageConnexion" class="flex md:ml-10 md:mr-16">
+                    <span id="logo" class="h-auto w-auto py-2 ">
                     <div class="flex flex-col items-center">
-                        <img src="media/images/LogoConnexion.png" alt="Connexion" width="100" height="100">
+                    <img src="media/images/LogoConnexion.png" alt="Connexion" width="100" height="100">
                     </div>
                     <p class="font-Roboto font-extrabold text-center text-white bg-primary rounded-md px-2">Se Connecter</p>
-                </span> 
-            </a>
+                    </span> 
+                </a>';
+        } else {
+            echo '<a href="?url=Connexion/Deconnexion" class="flex md:ml-10 md:mr-16">
+                    <span id="logo" class="h-auto w-auto py-2 ">
+                    <div class="flex flex-col items-center">
+                    <img src="media/images/LogoConnexion.png" alt="Connexion" width="100" height="100">
+                    </div>
+                    <p class="font-Roboto font-extrabold text-center text-white bg-primary rounded-md px-2">Se Deconnecter</p>
+                    </span> 
+                </a>';
+        }
+?>
 
         </nav>
     </header>
@@ -133,7 +146,7 @@
             <section>
                 <article>
                     <div id="message">
-                        <?= $message = isset($message) ? $message : NULL; ?>
+                        <?php $message = isset($message) ? $message : NULL; ?>
                     </div>
                     <div id="contenu">
                         <?= $contenu = isset($contenu) ? $contenu : NULL; ?>
@@ -143,12 +156,11 @@
             </section>
         </main>
     </div>
-
-    <footer class="mt-auto py-6 bg-gray-300 flex items-center justify-evenly md:flex-row flex-col text-lg">
-        <a class='pt-1' href="index.php?action=mention">Mention légales</a>
-        <p class='pt-1'>&copy; Le Bowling du Front de Mer</p>
-        <a class='pt-1' href="index.php?action=cookie">Cookies</a>
-    </footer>
+<footer class="mt-auto py-6 bg-gray-300 flex items-center justify-evenly md:flex-row flex-col text-lg">
+    <a class='pt-1' href="index.php?action=mention">Mention légales</a>
+    <p class='pt-1'>&copy; Le Bowling du Front de Mer</p>
+    <a class='pt-1' href="index.php?action=cookie">Cookies</a>
+</footer>
 </body>
 </html>
 
--- vue/vueProfil.php
+++ vue/vueProfil.php
@@ -1,10 +1,11 @@
 <?php
-session_start();
-if ($_SESSION['CONNECTER']=="OK") {
-    $titre = "Bowling du Front de Mer - Profil";
-    $contenu = "<h1>BONJOUR</h1>";
-    include "template.php";
-} else {
+    if ($_SESSION['CONNECTER']=="OK") 
+    {
+        $titre = "Bowling du Front de Mer - Profil";
+        $contenu = "<h1>BONJOUR</h1>";
+    } else {
+        include("vue/vueAcceuil");
+    }
     
-}
-?>
+    include "template.php";
+    ?>
