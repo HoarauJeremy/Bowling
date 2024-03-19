3ace8a26147c930dcbc9d54d123bf7df0f80a742
Tetrassky <tetrasky.bat@gmail.com>
2024-03-19 10:05:03 +0400

    Mentions légales

--- vue/vueMDPOublie.php
+++ vue/vueMDPOublie.php
@@ -1,22 +1,22 @@
 <?php
-require 'vendor/autoload.php'; // Inclure l'autoloader de Composer
+require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 $titre = "Mot de passe oublié - Bowling du Front de Mer";
 $contenu = "<div class='w-5/6 md:w-3/5 mx-auto text-center'>";
-$contenu .= "<form class='p-p7_5 border border-solid border-slate-50 bg-gray-200 shadow-xl h-auto flex flex-col items-center rounded-md mb-10' method='POST'>";
-$contenu .= "<img src='media/images/LogoConnexion.png' alt='Your Logo' width='120' height='120'>";
-$contenu .= "<br>";
-$contenu .= "<p><strong>Entrez votre adresse mail liée au compte existant.</strong></p>";
-$contenu .= "<input class='font-NotoSans text-xl h-20 w-1/2 px-3 py-4 pr-5 mt-5 mr-0 inline-block border-spacing-0.5 border-solid bg-gray-100 box-border' type='email' placeholder='Mon adresse' name='email'>";
-$contenu .= "<br>";
-$contenu .= "<div class='flex items-center justify-center w-1/2'>";
-$contenu .= "<input class='font-NotoSans text-xl h-20 w-full px-3 py-4 pr-5 mr-0 inline-block border-spacing-2 border-solid bg-gray-100 box-border' type='text' placeholder='Code de confirmation' name='code'>";
-$contenu .= "<button name='send_code' class='font-Roboto font-extrabold bg-red-800 text-white my-3.5 px-10 h-16 w-auto border-none cursor-pointer rounded-full hover:bg-red-900 hover:border-solid hover:border-spacing-0.5 ml-2'>Envoyer un code</button>";
+    $contenu .= "<form class='p-p7_5 border border-solid border-slate-50 bg-gray-200 shadow-xl h-auto flex flex-col items-center rounded-md mb-10' method='POST'>";
+    $contenu .= "<img src='media/images/LogoConnexion.png' alt='Your Logo' width='120' height='120'>";
+    $contenu .= "<br>";
+    $contenu .= "<p><strong>Entrez votre adresse mail liée au compte existant.</strong></p>";
+    $contenu .= "<input class='font-NotoSans text-xl h-20 w-1/2 px-3 py-4 pr-5 mt-5 mr-0 inline-block border-spacing-0.5 border-solid bg-gray-100 box-border' type='email' placeholder='Mon adresse' name='email'>";
+    $contenu .= "<br>";
+    $contenu .= "<div class='flex items-center justify-center w-1/2'>";
+    $contenu .= "<input class='font-NotoSans text-xl h-20 w-full px-3 py-4 pr-5 mr-0 inline-block border-spacing-2 border-solid bg-gray-100 box-border' type='text' placeholder='Code de confirmation' name='code'>";
+    $contenu .= "<button name='send_code' class='font-Roboto font-extrabold bg-red-800 text-white my-3.5 px-10 h-16 w-auto border-none cursor-pointer rounded-full hover:bg-red-900 hover:border-solid hover:border-spacing-0.5 ml-2'>Envoyer un code</button>";
 $contenu .= "</div>";
-$contenu .= "<br>";
+include "template.php";
 
 // Traitement pour l'envoi du mail de confirmation
 if(isset($_POST['send_code'])) {
--- vue/vueMention.php
+++ vue/vueMention.php
@@ -1,7 +1,37 @@
     <?php
     $titre = "Mention Légales - Bowling du Front de Mer";
 
-    $contenu = "bonjour";
+    $contenu = "<h1 class='text-5xl font-bold font-Roboto text-center'>MENTIONS LÉGALES</h1>";
+    $contenu .="<br>
+                <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l'économie numérique, il est précisé aux utilisateurs du site Bowling du Front de Mer l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+                
+                <br>
+                <h2 class='text-2xl font-bold font-Roboto'>Edition du site</h2>
+                Le présent site, accessible à l’URL BowlingDuFrontDeMer.alwaysdata.net (le « Site »), est édité par :
+                <br>
+                <p class='p-12'>Bowling du Front de Mer , société au capital de 225000 euros, inscrite au R.C.S. de SAINT-DENIS-DE-LA-REUNION sous le numéro 934612576, dont le siège social est situé au 12 Rue Hubert Delisle, Saint-Denis 97400, La Réunion, représenté(e) par Jean-Pierre Marchais dûment habilité(e)
+                Le numéro individuel TVA de l’éditeur est : 123456789.</p>
+
+                <br>
+                <h2 class='text-2xl font-bold font-Roboto'>Hébergement</h2>
+                <p class='p-12'>Le Site est hébergé par la société Always Data, situé 91 rue du Faubourg Saint Honoré 75008 Paris, (contact téléphonique ou email : +33184162340).</p>
+                
+                <br>
+                <h2 class='text-2xl font-bold font-Roboto'>Directeur de publication</h2>
+                <p class='p-12'>Le Directeur de la publication du Site est Jean-Pierre Marchais.</p>
+
+                <br>
+                <h2 class='text-2xl font-bold font-Roboto'>Nous contacter </h2>
+                <p class='p-12'>Par téléphone : 06 92 00 00 00</p>
+                <p class='p-12'>Par email : contact@bowlingdufrontdemer.re</p>
+                <p class='p-12'>Par courrier : 12 Rue Hubert Delisle, Saint-Denis 97400, La Réunion</p>
+
+                <br>
+                <h2 class='text-2xl font-bold font-Roboto'>Données personnelles</h2>
+                <p>Le traitement de vos données à caractère personnel est régi par notre Charte du respect de la vie privée, disponible depuis la section 'Charte de Protection des Données Personnelles', conformément au Règlement Général sur la Protection des Données 2016/679 du 27 avril 2016 («RGPD»).</p>
+                
+                <br>
+                ";         
 
     include("template.php");
 ?>
