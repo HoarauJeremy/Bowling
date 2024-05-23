c66dacddeaddfcfbb31eb187a24f68338354d234
jeremy <jeremyhoarau44@gmail.com>
2024-05-23 13:51:48 +0400

    MàJ de fichiers

--- controler/controleurReservations.php
+++ controler/controleurReservations.php
@@ -11,49 +11,64 @@ class controleurReservations
     }
 
     public function Reservations () {
-        $reservations = $this->manageReservations->getReservations();
-        include 'vue/vueReservations.php';
+        if ($_SESSION['type'] == 2 && $_SESSION['CONNECTER'] == "OK") {
+            $reservations = $this->manageReservations->getReservations();
+            include 'vue/vueReservations.php';
+        } else {
+            include 'vue/vueAccueil.php';
+        }
     }
 
     public function reservation ($id) {
-        $reservation = $this->manageReservations->getRes($id);
-        if ($reservation != null) {
-            require_once("modele/administration.php");
-            $res = new administration($reservation);
-            include 'vue/vueReservation.php';
+        if ($_SESSION['type'] == 2 && $_SESSION['CONNECTER'] == "OK") {
+            $reservation = $this->manageReservations->getRes($id);
+            if ($reservation != null) {
+                require_once("modele/administration.php");
+                $res = new administration($reservation);
+                include 'vue/vueReservation.php';
+            } else {
+                include 'vue/vueErreur.php';
+            }
         } else {
-            include 'vue/vueErreur.php';
+            include 'vue/vueAccueil.php';
         }
     }
 
     public function addReservation() {
-        $currentDateTime = new DateTime('now');
-        $currentDate = $currentDateTime->format('Y-m-d H:m:s');
+            $currentDateTime = new DateTime('now');
+            $currentDate = $currentDateTime->format('Y-m-d H:m:s');
 
-        $data = array($_POST['NbPers'],$currentDate,$_POST['HD'],$_POST['HF'],$_POST['Formule'],$_POST['mailClient']);
-        $reservation = $this->manageReservations->addReservation($data);
-        include 'vue/vueAllReservations.php';
-
+            $data = array($_POST['NbPers'],$currentDate,$_POST['HD'],$_POST['HF'],$_POST['Formule'],$_POST['mailClient']);
+            $reservation = $this->manageReservations->addReservation($data);
+            include 'vue/vueAllReservations.php';
     }
 
     public function modifier ($id) {
-        $element = $this->manageReservations->getRes($id);
-        if ($element != null) {
-            require_once("modele/administration.php");
-            $modifreservation = new administration($element);
-            include 'vue/vueModifAdmin.php';
+        if ($_SESSION['CONNECTER'] == "OK") {
+            $element = $this->manageReservations->getRes($id);
+            if ($element != null) {
+                require_once("modele/administration.php");
+                $modifreservation = new administration($element);
+                include 'vue/vueModifAdmin.php';
+            } else {
+                include 'vue/vueErreur.php';
+            }
         } else {
-            include 'vue/vueErreur.php';
+            include 'vue/vueConnexion.php';
         }
     }
 
     public function supprimer ($id) {
-        $element = $this->manageReservations->deleteReservation($id);
-        if ($element != null) {
-            $message = "La réservation n° ".$id." a été supprimé avec succès!";
-            header('Location: index.php?url=Reservations/');
+        if ($_SESSION['CONNECTER'] == "OK") {
+            $element = $this->manageReservations->deleteReservation($id);
+            if ($element != null) {
+                $message = "La réservation n° ".$id." a été supprimé avec succès!";
+                header('Location: index.php');
+            } else {
+                include 'vue/vueReservations.php';
+            }
         } else {
-            include 'vue/vueReservations.php';
+            include 'vue/vueConnexion.php';
         }
     }
 
